7d8c689d1f0c337aa20dc95f6a565c4a60a4c9c6
sreedeepakchintalapudi101 <78522799+sreedeepakchintalapudi101@users.noreply.github.com>
2024-10-26 19:38:58 +0530

    Update business_rules_api 1 (1).py

--- business_rules_api 1 (1).py
+++ business_rules_api 1 (1).py
@@ -1,26 +1,10 @@
-import os
-import json
-import pandas as pd
-import ast
-import psutil
-import requests
-
-from flask import Flask, request, jsonify
-from db_utils import DB
-from ace_logger import Logging
-from app import app
-#
-from .BusinessRules import BusinessRules
-from time import time as tt
-from py_zipkin.zipkin import zipkin_span, ZipkinAttrs
-from py_zipkin.util import generate_random_64bit_string
-
-logging = Logging(name='business_rules_api')
-
-# Database configuration
-db_config = {
-    'host': os.environ['HOST_IP'],
-    'user': os.environ['LOCAL_DB_USER'],
-    'password': os.environ['LOCAL_DB_PASSWORD'],
-    'port': os.environ['LOCAL_DB_PORT'],
-}
+def http_transport(encoded_span):
+    # The collector expects a thrift-encoded list of spans. Instead of
+    # decoding and re-encoding the already thrift-encoded message, we can just
+    # add header bytes that specify that what follows is a list of length 1.
+    body = encoded_span
+    requests.post(
+        'http://servicebridge:80/zipkin',
+        data=body,
+        headers={'Content-Type': 'application/x-thrift'},
+    )
